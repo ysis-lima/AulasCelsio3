a6798e05ea44f8a8dff5e1dc08d42d932e95bfa3
Ysis Lima <132459234+ysis-lima@users.noreply.github.com>
2024-05-08 09:32:49 -0300

    Alura - Aula 04

--- TypeNumber.js
+++ TypeNumber.js
@@ -3,6 +3,10 @@
 const meuNumero = 3;
 const primeiroNum = 1;
 const segundoNum = 2;
-const operacaoMat = primeiroNum + segundoNum;
+
+/*const operacaoMat = primeiroNum + segundoNum;*/
+/*const operacaoMat = primeiroNum * segundoNum;*/
+/*const operacaoMat = primeiroNum / segundoNum;*/
+const operacaoMat = primeiroNum - segundoNum;
 
 console.log(operacaoMat)
