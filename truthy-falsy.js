5a4b287f2df865872b2ba1a1dcdeaa786e4a6864
Ysis Lima <132459234+ysis-lima@users.noreply.github.com>
2024-05-12 18:35:09 -0300

    Alura - aula 03 (02)

---
+++ truthy-falsy.js
@@ -0,0 +1,28 @@
+// boolean
+
+const ususarioLogado = true;
+const contaPaga = false;
+
+// truthy or falsy
+
+// 0 == false;
+// 1 == true;
+
+/*console.log(0==false)
+console.log(""==false)
+console.log(1==true)*/
+
+//null
+//undefined
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar)
+console.log(varNull)
+
+let numero = 3;
+let texto = 'Castelo';
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
