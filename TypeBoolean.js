735d73ec9ab4dd4c9aa9f21da911cd572a50d191
Ysis Lima <132459234+ysis-lima@users.noreply.github.com>
2024-05-12 17:46:36 -0300

    Alura - aula 09.

---
+++ TypeBoolean.js
@@ -0,0 +1,11 @@
+const priNum = 5;
+/*const segNum = 10;*/
+const segNum = 5;
+let cadastroAtivado = true;
+
+/*console.log(priNum === segNum)*/
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2)
