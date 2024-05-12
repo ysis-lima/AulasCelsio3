f860b29bb50991dcef9b4dc5b4289ef976f1f565
Ysis Lima <132459234+ysis-lima@users.noreply.github.com>
2024-05-12 18:34:28 -0300

    Alura - aula 02 (02)

---
+++ Script.js
@@ -0,0 +1,42 @@
+//var
+
+/*var altura = 5;
+var comprimento = 7;
+
+area = (altura * comprimento);
+console.log(area)
+var area;*/
+
+//let
+
+/*let forma = 'Retângulo'
+let altura = 5;
+let comprimento = 7;
+let area;
+
+if(forma === 'Retângulo'){
+    area = (altura * comprimento);
+}
+else{
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area)*/
+
+//const
+
+const forma = 'Triangulo'
+const altura = 5;
+const comprimento = 7;
+let area;
+
+
+if (forma === "Quadrado"){
+    area = altura * comprimento;
+}
+else{
+    area = (altura * comprimento) / 2;
+}
+
+
+console.log(area)
