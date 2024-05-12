f44784df7abe14c2d33561e63e4b89ca0d9c836c
Ysis Lima <132459234+ysis-lima@users.noreply.github.com>
2024-05-12 17:46:05 -0300

    Alura - aula 06.

---
+++ TypeString.js
@@ -0,0 +1,12 @@
+const Text1 = "Olá mundo!";
+const Text2 = 'Olá mundo!';
+const senha = "senhaSuperSegura456";
+const StringdeNumeros = "3456";
+
+const citacao = 'E o corvo disse ';
+const fala = '"Nunca mais!".'
+
+//concatenção (+)
+console.log(citacao + fala)
+
+//template string OU tamplate simple
