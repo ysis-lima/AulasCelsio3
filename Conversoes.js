c87e265671266d04f4dc78e883519ee951665e44
Ysis Lima <132459234+ysis-lima@users.noreply.github.com>
2024-05-13 09:19:46 -0300

    Alura - aula 04 (02)

---
+++ Conversoes.js
@@ -0,0 +1,18 @@
+//tipo de dado
+//booleanos
+
+//conversão implícita
+/*const numero = 456;
+const numeroString = "456";
+
+console.log(numero == numeroString)*/
+// === compara valor e tipo
+// == compara valor
+
+//conversão exlplícita
+const numero = 456;
+const numeroString = Number("456");
+
+// Number()
+// String()
+console.log(numero + numeroString)
