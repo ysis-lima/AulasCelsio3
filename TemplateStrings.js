20a96fbdcaac1167950b95805060d7b882d27d14
Ysis Lima <132459234+ysis-lima@users.noreply.github.com>
2024-05-14 16:37:25 -0300

    Alura - aula 05 (04)

---
+++ TemplateStrings.js
@@ -0,0 +1,10 @@
+const nome = "Drácula";
+const idade = 2023-1882
+const cidadeNatal = "Salém"
+
+/*const apresentacao = "Meu nome é " + nome + 
+", minha idade é " + idade + ", nasci em " + 
+cidadeNatal;*/
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade}, nasci em ${cidadeNatal}.`;
+console.log(apresentacao)
