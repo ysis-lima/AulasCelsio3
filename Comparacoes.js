86bd1b6045995f4d39d812f04e121d216c0f51fd
Ysis Lima <132459234+ysis-lima@users.noreply.github.com>
2024-05-14 16:35:12 -0300

    Alura - aula 02 (04)

---
+++ Comparacoes.js
@@ -0,0 +1,18 @@
+// == (comparação implícita)
+
+const num = 5;
+const text = '5';
+
+/*console.log(num == text)*/
+console.log(num === text)
+
+// type of
+console.log(typeof num)
+console.log(typeof text)
+
+// == compara apenas o valor
+// === compara o valor e o tipo
+
+// Conversão explícita
+Number()
+String()
