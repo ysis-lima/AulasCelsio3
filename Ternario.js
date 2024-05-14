0de4dd7b22abc26defbee551a48200295f0c735e
Ysis Lima <132459234+ysis-lima@users.noreply.github.com>
2024-05-14 16:36:21 -0300

    Alura - aula 04 (04)

---
+++ Ternario.js
@@ -0,0 +1,14 @@
+
+const idadeMin = 18;
+const idadeCliente = 44;
+
+/*if (idadeCliente >= idadeMin){
+    console.log("Vinho")
+}
+else {
+    console.log("Suco")
+}*/
+
+//com o tenário...
+            //condição                  //true  // false
+console.log( idadeCliente >= idadeMin ? "Vinho" : "Suco")
