b8f6aa819b8d2062bde000c15f1f0aa1c19c3486
Ysis Lima <132459234+ysis-lima@users.noreply.github.com>
2024-05-22 09:20:47 -0300

    Alura - 04 (02)

---
+++ Objetos/Aula2/aninhados.js
@@ -0,0 +1,15 @@
+const cliente = {
+    nome: "Suzume Nana",
+    idade: 19,
+    email: "suzume-nana@empresa.com",
+    telefone: ["12 3333-3333", "12 4444-4444"] ,
+};
+
+cliente.endereco = {
+    rua: "R. Kitsune",
+    numero: 2222,
+    apartamento: true,
+    complemento: "ap 222",
+};
+
+console.log(cliente.endereco);
