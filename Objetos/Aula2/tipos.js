10ad32140462089777c96743291bab45f003d6b1
Ysis Lima <132459234+ysis-lima@users.noreply.github.com>
2024-05-22 08:57:03 -0300

    Alura - aula 02 (02)

---
+++ Objetos/Aula2/tipos.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome: "Suzume Nana",
+    idade: 19,
+    email: "suzume-nana@empresa.com",
+    telefone: ["12 3333-3333", "12 4444-4444"] ,
+};
+
+console.log(cliente.telefone);
